14a02a6015e815d981cb0f7e7e4c34595b83e98f
Val <>
2020-06-23 20:30:31 +0300

    fix modul

--- shop/app/models/user.rb
+++ shop/app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
   has_many :orders
 
+  devise :database_authenticatable
+
   def self.search(params)
     user = all
     user = user.where("name LIKE ?", "%#{params[:search]}%") if params[:search]
     user
   end
-  
+
   def self.authenticate(name, password)
 		user = find_by_name(name)
 		if user && user.password == password
--- shop/config/routes.rb
+++ shop/config/routes.rb
@@ -1,10 +1,28 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :product_orders
   resources :orders
+
+  resources :orders do
+    resources :product_orders
+  end
+
   resources :users
+  resources :users do
+    resources :orders
+  end
+
   resources :products
+  resources :products do
+    resources :product_orders
+  end
 
   root 'products#index'
+
+  get "log_out" => "sessions#destroy", :as => "log_out"
+	get "log_in" => "sessions#new", :as => "log_in"
+	get "sign_up" => "users#new", :as => "sign_up"
+	get "account/:id" => "users#edit", :as => "account"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
